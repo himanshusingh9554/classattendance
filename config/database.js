1d815626998aa4123f9ec1515baa8b20dfaff1f3
()={Developer} <75067287+himanshusingh9554@users.noreply.github.com>
2025-02-26 23:58:08 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize('attendance_db', 'root', 'rootnode', {
   host: 'localhost',
   dialect: 'mysql',
+dialectModule: mysql2
+
 });
 
 export default sequelize;
